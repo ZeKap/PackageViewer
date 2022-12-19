232e86eaf71a640f5d5b14cf4229d996d543d0cd
ZeKap <kapismath@gmail.com>
2022-12-19 12:10:38 +0100

    starting resizer js

---
+++ resizer.js
@@ -0,0 +1,20 @@
+// get resizer
+const slider = document.querySelector("#resizer");
+
+// values that are shared between the 3 functions below
+var mouseX = 0;
+var before;
+var beforeSize;
+var resizer;
+
+function mouseDownHandler(e) {
+
+}
+
+function mouseMoveHandler(e) {
+
+}
+
+function mouseUpHandler(e) {
+
+}
